f25cc2b5577f67fce57664049970491afeb4a14a
Elysia1234567 <2763843393@qq.com>
2025-06-07 00:27:33 +0800

    修改config

--- server/src/main/java/com/example/configuration/CorsConfig.java
+++ server/src/main/java/com/example/configuration/CorsConfig.java
@@ -1,16 +1,26 @@
 package com.example.configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 对所有接口生效
-                .allowedOriginPatterns("*") // 允许的源（生产环境建议精准配置，如前端域名）
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 允许的请求方法
-                .allowedHeaders("*") // 允许的请求头
-                .allowCredentials(true); // 是否允许携带凭证（如cookie）
+public class CorsConfig {
+    public CorsConfig() {
+        System.out.println("CorsConfig 被初始化加载"); // 启动时看控制台是否输出
+    }
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
