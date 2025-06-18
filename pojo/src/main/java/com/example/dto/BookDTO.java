50572c5882488bd1d453cf7786909a910232ed45
JH731 <140873770+JH731@users.noreply.github.com>
2025-06-19 00:54:16 +0800

    Update BookDTO.java

--- pojo/src/main/java/com/example/dto/BookDTO.java
+++ pojo/src/main/java/com/example/dto/BookDTO.java
@@ -11,17 +11,28 @@ public class BookDTO {
     //书籍名称
     private String name;
     //书籍分类id
-    private Integer categoryId;
-    //分类名称
     private String categoryName;
+
+    //作者
+    private String author;
+    //出版社
+    private String publish;
+    //第几版的书
+    private String edition;
     //图片
     private String image;
-    //描述信息
-    private String description;
     //借阅状态:0 不可借阅  1 可借阅
     private Integer status;
     //数量
     private Integer stock;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+
+    private Integer createUser;
+
+    private Integer updateUser;
 
 
 
