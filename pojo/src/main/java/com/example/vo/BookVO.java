eee5a42f866d3933d870e2c9c35bfa107833932f
JH731 <140873770+JH731@users.noreply.github.com>
2025-06-19 00:28:41 +0800

    Update BookVO.java

--- pojo/src/main/java/com/example/vo/BookVO.java
+++ pojo/src/main/java/com/example/vo/BookVO.java
@@ -1,8 +1,14 @@
 package com.example.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookVO {
     private Integer id;
     //书籍名称
