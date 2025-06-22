fdd8592e686809d7d367312baa0a335d8890b515
JH731 <140873770+JH731@users.noreply.github.com>
2025-06-22 16:13:26 +0800

    Update BorrowMapper.java

--- server/src/main/java/com/example/mapper/BorrowMapper.java
+++ server/src/main/java/com/example/mapper/BorrowMapper.java
@@ -13,16 +13,18 @@ import java.util.List;
 
 @Mapper
 public interface BorrowMapper {
-    @Select("SELECT bw.*, bk.name as bookName, bk.author as author, bk.publish as publish, bk.edition as edition, ct.name AS categoryName " +
+    @Select("SELECT bw.*, bk.name as bookName, bk.author as author, bk.publish as publish, bk.edition as edition, ct.name AS categoryName, bu.name as userName " +
             "FROM book_borrow.borrow bw " +
             "LEFT JOIN book_borrow.book bk ON bw.book_id = bk.id " +
             "LEFT JOIN book_borrow.category ct ON bk.category_id = ct.id " +
+            "LEFT JOIN book_borrow.user bu ON bw.user_id = bu.id " +
             "WHERE 1 = 1 " +
             "  AND (#{userId} IS NULL OR bw.user_id = #{userId}) " +
+            "  AND (#{userName} IS NULL OR #{userName} = '' OR bu.name = #{userName}) " +
+            "  AND (#{categoryName} IS NULL OR #{categoryName} = '' OR ct.name = #{categoryName}) " +
             "  AND (#{categoryId} IS NULL OR bk.category_id = #{categoryId}) " +
             "  AND (#{startTime} IS NULL OR bw.start_time = #{startTime}) " +
-            "  AND (#{endTime} IS NULL OR bw.end_time = #{endTime}) " +
-            "  AND (#{returnTime} IS NULL OR bw.return_time = #{returnTime}) ")
+            "  AND (#{endTime} IS NULL OR bw.end_time = #{endTime}) ")
     Page<BorrowVO> pageQuery(BorrowQueryDTO borrowQueryDTO);
 
 
@@ -34,15 +36,9 @@ public interface BorrowMapper {
             "SET " +
             "status = CASE WHEN #{status} IS NOT NULL THEN #{status} ELSE status END, " +
             "user_id = CASE WHEN #{userId} IS NOT NULL THEN #{userId} ELSE user_id END, " +
-            "book_id = CASE WHEN #{bookId} IS NOT NULL THEN #{bookId} ELSE book_id END, " +
-            "return_time = CASE " +
-            "   WHEN #{status} = 1 THEN NOW() " +
-            "   WHEN #{status} = 2 THEN NULL " +
-            "   ELSE return_time " +
-            "END " +
+            "book_id = CASE WHEN #{bookId} IS NOT NULL THEN #{bookId} ELSE book_id END " +
             "WHERE id = #{id}")
     @AutoFill(value = OperationType.UPDATE)
-    //todo returnTime还没有进行处理
     void update(Borrow borrow);
 
     @Select("SELECT bw.*, bk.name as bookName, bk.author as author, bk.publish as publish, bk.edition as edition, ct.name AS categoryName, bu.name as userName " +
