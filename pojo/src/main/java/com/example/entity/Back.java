3eeb4ed7ca7f58aaf38527e7b034c82b42827450
JH731 <140873770+JH731@users.noreply.github.com>
2025-06-25 16:56:25 +0800

    Update Back.java

--- pojo/src/main/java/com/example/entity/Back.java
+++ pojo/src/main/java/com/example/entity/Back.java
@@ -2,10 +2,19 @@ package com.example.entity;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class Back {
     private Integer id;
     private Integer brid;
     //点击了这个按钮之后就是归还状态了,所以默认设置为0
     private Integer status = 0;
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+
+    private Integer createUser;
+
+    private Integer updateUser;
 }
